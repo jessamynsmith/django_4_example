a0b65c369640822e21bbb48f6a74ebdeee0e47a3
jessamynsmith <geekchick77@gmail.com>
2022-12-22 19:52:57 -0700

    Only show latest log for each transaction_id

--- my_app/admin.py
+++ my_app/admin.py
@@ -3,4 +3,8 @@ from django.contrib import admin
 from my_app import models
 
 
-admin.site.register(models.TransactionLog)
+class TransactionLogAdmin(admin.ModelAdmin):
+    list_display = ['id', 'transaction_id', 'notes', 'created_at']
+
+
+admin.site.register(models.TransactionLog, TransactionLogAdmin)
--- my_app/views.py
+++ my_app/views.py
@@ -7,6 +7,11 @@ from my_app import models as my_app_models
 class TransactionLogListView(ListView):
     model = my_app_models.TransactionLog
 
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        queryset = queryset.order_by('transaction_id', '-id').distinct('transaction_id')
+        return queryset
+
 
 class TransactionLogCreateView(CreateView):
     model = my_app_models.TransactionLog
