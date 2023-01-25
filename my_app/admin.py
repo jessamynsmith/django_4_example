ac31c0a94e76c7b40d95738d6e8e1eba44746a80
jessamynsmith <geekchick77@gmail.com>
2023-01-24 19:03:52 -0700

    Make read only admin.

--- my_app/admin.py
+++ my_app/admin.py
@@ -3,7 +3,15 @@ from django.contrib import admin
 from my_app import models
 
 
-class TransactionLogAdmin(admin.ModelAdmin):
+class ReadonlyMixin:
+    def has_change_permission(self, request, obj=None):
+        return False
+
+    def has_delete_permission(self, request, obj=None):
+        return False
+
+
+class TransactionLogAdmin(ReadonlyMixin, admin.ModelAdmin):
     list_display = ['id', 'transaction_id', 'notes', 'created_at']
 
 
